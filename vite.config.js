2683ee4db1b7d1fb774887571dfb5c9088c775ea
hhh <147622825+hzonz@users.noreply.github.com>
2026-05-20 11:03:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,18 @@ export default defineConfig({
     outDir: ".", 
     emptyOutDir: false,
     minify: "terser",
+    terserOptions: {
+      compress: { 
+        drop_console: true, // 生产环境移除 console
+        drop_debugger: true 
+      }
+    },
+    rollupOptions: {
+      // 如果你想让文件更小，可以取消下面三行的注释
+      // external: [/^lit/], 
+      // output: {
+      //   globals: { lit: "lit" }
+      // }
+    }
   },
 });
